6e5bf8e465e4da17f2832ce3963387063a111fb2
giventech <giventech@gmail.com>
2020-09-08 19:29:31 +1000

    added NX Style modules and libraries

--- apps/prioritisation-e2e/src/integration/app.spec.ts
+++ apps/prioritisation-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('techprecedence', () => {
+describe('prioritisation', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('techprecedence', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to techprecedence!');
+    getGreeting().contains('Welcome to prioritisation!');
   });
 });
---
+++ libs/prioritisation/data-access/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/prioritisation-data-access.module';
