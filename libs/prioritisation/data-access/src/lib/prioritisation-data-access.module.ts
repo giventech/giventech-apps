d3da035fc69eda7d5795ef843498d6111104c8b7
giventech <giventech@gmail.com>
2020-10-21 07:49:07 +1100

    refactor(prioritisation): added configuration

--- libs/prioritisation/data-access/src/lib/prioritisation-data-access.module.ts
+++ libs/prioritisation/data-access/src/lib/prioritisation-data-access.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [CommonModule],
+	imports: [CommonModule],
 })
-
 export class PrioritisationDataAccessModule {}
